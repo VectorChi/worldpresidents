d485bb20379cbd60a343c2dcaf182e28f71b778a
Agung Sundoro <muhagung2001@gmail.com>
2023-01-03 20:49:44 +0700

    chore: revert main.js to original version

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 // Making reference
+const searchInput = document.querySelector("#search");
+const listItem = document.querySelectorAll(".list_wrapper .row");
 const toggleIcon = document.querySelector("#theme_icon");
 
-/** Theme Toggle (Dark/Light) */
+// search funtion
+searchInput.addEventListener("keyup", (e) => {
+  // initializing input value
+  const searchChar = e.target.value.toLowerCase();
+  listItem.forEach((rowItem) => {
+    // specify filter by country and capital
+    let searchTarget = rowItem.querySelector(".country_Capital").textContent;
+    console.log(searchTarget);
+
+    if (searchTarget.toLowerCase().includes(searchChar)) {
+      rowItem.style.display = "";
+    } else {
+      rowItem.style.display = "none";
+    }
+  });
+});
+
+//theme toggle
 toggleIcon.addEventListener("click", (e) => {
   document.body.classList.toggle("dark_theme");
 
