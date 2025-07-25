7ae3d23766b15e2d1cf7b9579057bdc63c013ab3
Adimchi <adimchisylvester2@gmail.com>
2025-07-25 08:49:12 +0100

    added president-country JSON data & render function

--- main.js
+++ main.js
@@ -30,30 +30,31 @@ toggleIcon.addEventListener("click", (e) => {
   } else toggleIcon.src = "./assets/moon.svg";
 });
 
-//load more funtion
-let Row = document.querySelectorAll(".row"),
-  Load = document.querySelector(".load"),
-  currentList = 0;
+// // load more funtion
+// let Row = document.querySelectorAll(".row"),
+//   Load = document.querySelector(".load"),
+//   currentList = 0;
 
-Load.addEventListener("click", () => {
-  for (let i = currentList; i < currentList + 20; i++) {
-    if (Row[i]) {
-      Row[i].style.display = "grid";
-    }
-  }
-  currentList += 20;
-  if (currentList >= Row.length) {
-    e.target.style.display = "none";
-  }
+// Load.addEventListener("click", (e) => {
+//   for (let i = currentList; i < currentList + 20; i++) {
+//     if (Row[i]) {
+//       Row[i].style.display = "grid";
+//     }
+//   }
+//   currentList += 20;
+//   if (currentList >= Row.length) {
+//     e.target.style.display = "none";
+//   }
+// });
+
+goUp.addEventListener("click", (e) => {
+  document.querySelector("nav").scrollIntoView({ behavior: "smooth" });
 });
 
-goUp.addEventListener("click", () => {
-  document.querySelector("nav").scrollIntoView({ behavior: 'smooth' });
-})
 //function to find ages and increment 1 after 365 days
 function incrementPresidentAgesOnceAfterOneYear() {
   function incrementAges() {
-    document.querySelectorAll('.age').forEach(ageDiv => {
+    document.querySelectorAll(".age").forEach((ageDiv) => {
       let match = ageDiv.textContent.match(/^(\d+)\s*years$/);
       if (match) {
         let newAge = parseInt(match[1], 10) + 1;
@@ -66,4 +67,7 @@ function incrementPresidentAgesOnceAfterOneYear() {
 }
 
 // Call this function after the page loads
-window.addEventListener('DOMContentLoaded', incrementPresidentAgesOnceAfterOneYear);
+window.addEventListener(
+  "DOMContentLoaded",
+  incrementPresidentAgesOnceAfterOneYear
+);
